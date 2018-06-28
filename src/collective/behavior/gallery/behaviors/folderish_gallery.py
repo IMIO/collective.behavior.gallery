d960ad0e1d94758f77dcba8a6197aefa199fa770
Nicolas Demonte <nicolas@affinitic.be>
2018-06-28 18:32:18 +0200

    Working prototype

--- src/collective/behavior/gallery/behaviors/folderish_gallery.py
+++ src/collective/behavior/gallery/behaviors/folderish_gallery.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from collective.behavior.gallery import _
-from plone import schema
 from plone.autoform.interfaces import IFormFieldProvider
-from plone.dexterity.interfaces import IDexterityContent
+from plone.dexterity.interfaces import IDexterityContainer
 from plone.supermodel import model
 from zope.component import adapter
 from zope.interface import implementer
@@ -14,26 +12,11 @@ from zope.interface import provider
 class IFolderishGallery(model.Schema):
     """
     """
-
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
-        required=False,
-    )
+    pass
 
 
 @implementer(IFolderishGallery)
-@adapter(IDexterityContent)
+@adapter(IDexterityContainer)
 class FolderishGallery(object):
     def __init__(self, context):
         self.context = context
-
-    @property
-    def project(self):
-        if hasattr(self.context, 'project'):
-            return self.context.project
-        return None
-
-    @project.setter
-    def project(self, value):
-        self.context.project = value
