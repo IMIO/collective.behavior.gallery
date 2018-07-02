2905c14efe0ccec1ca8906914d1593702b75a678
Nicolas Demonte <nicolas@affinitic.be>
2018-07-02 11:10:10 +0200

    Fix some pre-commit warnings

--- src/collective/behavior/gallery/tests/test_behavior_folderish_gallery.py
+++ src/collective/behavior/gallery/tests/test_behavior_folderish_gallery.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from collective.behavior.gallery.behaviors.folderish_gallery import IFolderishGallery
+from collective.behavior.gallery.behaviors.folderish_gallery import IFolderishGallery # noqa
 from collective.behavior.gallery.testing import COLLECTIVE_BEHAVIOR_GALLERY_INTEGRATION_TESTING  # noqa
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
@@ -19,12 +19,15 @@ class FolderishGalleryIntegrationTest(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_behavior_folderish_gallery(self):
-        behavior = getUtility(IBehavior, 'collective.behavior.gallery.folderish_gallery')
+        behavior = getUtility(
+            IBehavior,
+            'collective.behavior.gallery.folderish_gallery'
+        )
         self.assertEqual(
             behavior.marker,
             IFolderishGallery,
         )
-        behavior_name = 'collective.behavior.gallery.behaviors.folderish_gallery.IFolderishGallery'
+        behavior_name = 'collective.behavior.gallery.behaviors.folderish_gallery.IFolderishGallery' # noqa
         behavior = getUtility(IBehavior, behavior_name)
         self.assertEqual(
             behavior.marker,
