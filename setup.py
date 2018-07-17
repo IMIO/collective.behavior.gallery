ff38e2d123096028a5a48f9cc15974ff7d534f6b
Nicolas Demonte <nicolas@affinitic.be>
2018-07-17 15:12:45 +0200

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.gallery',
-    version='1.0a4',
+    version='1.0a5.dev0',
     description="Add a gallery of images to Folderish content types",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
